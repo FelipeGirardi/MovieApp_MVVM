27a788d7e755aa652f51e494bf8e1b252ad19595
Felipe Girardi <felipegirardifg@gmail.com>
2019-08-16 16:15:02 -0300

    Add API request (initial)

--- MovieApp_MVVM/API Service/API.swift
+++ MovieApp_MVVM/API Service/API.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+struct DataService {
+    
+    static func saveNowPlayingMovies() {
+        
+        let urlString = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=fb61737ab2cdee1c07a947778f249e7d&page=1")
+        
+        if let url = urlString {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                } else {
+                    if let jsonData = data {
+                        do {
+                            let jsonDecoder = JSONDecoder()
+                            let modelData = try jsonDecoder.decode(Movies.self, from: jsonData)
+                            print(modelData)
+                            
+                        } catch {
+                            print("JSON Processing Fail")
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+        
+        
+    }
+    
+}
+
+
--- MovieApp_MVVM/Model/MovieModel.swift
+++ MovieApp_MVVM/Model/MovieModel.swift
@@ -8,12 +8,62 @@
 
 import Foundation
 
-struct Movie {
+// MARK: - Movies
+struct Movies: Codable {
+    let results: [Result]
+    let page, totalResults: Int
+    let dates: Dates
+    let totalPages: Int
     
-    let genre_ids: [Int]
-    let overview: String
-    let original_title: String
-    let vote_average: Float
-    let poster_path: String
-    
+    enum CodingKeys: String, CodingKey {
+        case results, page
+        case totalResults = "total_results"
+        case dates
+        case totalPages = "total_pages"
+    }
 }
+
+// MARK: - Dates
+struct Dates: Codable {
+    let maximum, minimum: String
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let voteCount, id: Int
+    let video: Bool
+    let voteAverage: Double
+    let title: String
+    let popularity: Double
+    let posterPath: String
+    let originalLanguage: OriginalLanguage
+    let originalTitle: String
+    let genreIDS: [Int]
+    let backdropPath: String
+    let adult: Bool
+    let overview, releaseDate: String
+    
+    enum CodingKeys: String, CodingKey {
+        case voteCount = "vote_count"
+        case id, video
+        case voteAverage = "vote_average"
+        case title, popularity
+        case posterPath = "poster_path"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case genreIDS = "genre_ids"
+        case backdropPath = "backdrop_path"
+        case adult, overview
+        case releaseDate = "release_date"
+    }
+}
+
+enum OriginalLanguage: String, Codable {
+    case en = "en"
+    case ja = "ja"
+    case ko = "ko"
+}
+
+
+
+
